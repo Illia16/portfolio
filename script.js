6f851f2def55cd48532d41206bf5d85cdca33c5b
Illia <illia2143@gmail.com>
2020-09-11 00:31:36 -0400

    more work done

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         $('#nav').toggleClass('openedNav');
         $(`.showNav span`).toggleClass('clicked');
     });
+
+    $("a.scrollTo").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+
+        $('#nav').removeClass('openedNav');
+        $(`.showNav span`).removeClass('clicked');
+    });
 });
