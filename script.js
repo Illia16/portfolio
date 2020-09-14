e9de0120aa9dcc9246e13658ee76210e1c6366b8
Illia <illia2143@gmail.com>
2020-09-14 00:53:47 -0400

    done. gonna work on more features/improvements later

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     $('#openNav').click(function() {
         $('#nav').toggleClass('openedNav');
         $(`.showNav span`).toggleClass('clicked');
+
+        $(`.headerTop .logoInitials`).toggleClass('toggleOpacity');
     });
 
     // WHEN CLICKING ON A CERTAIN LINK WE GO TO the #element
@@ -13,32 +15,50 @@ $(document).ready(function(){
 
         $('#nav').removeClass('openedNav');
         $(`.showNav span`).removeClass('clicked');
+
+        // bringing back our logo initials to 1(visible)
+        $(`.headerTop .logoInitials`).toggleClass('toggleOpacity');
+        //$(`.headerTop .logoInitials`).css({'opacity': '1'});
     });
 
-    console.log(document.documentElement);
-    console.log(window.scrollY);
-    console.log(window);
-    console.log($("header").height());
-    console.log($("header"));
+    $(".sortButtons button").click(function () {
+        $(`.${this.value}`).delay("500").slideDown();
+        $(`.projectsList li`).not(`.${this.value}`).slideUp();
+        this.value === 'all' && $(`.projectsList li`).delay("500").slideDown();
+    });
 
 
-    // CHECK IF STICKY TOUCHES TOP?
-    // var observer = new IntersectionObserver(function(entries) {
-    //     // no intersection with screen
-    //     if(entries[0].intersectionRatio === 0) {
-    //         document.querySelector("#nav-container").classList.add("nav-container-sticky");
-    //         console.log("no intersection with screen"); 
-    //     }
-    //     // fully intersects with screen
-    //     else if(entries[0].intersectionRatio === 1)
-    //         document.querySelector("#nav-container").classList.remove("nav-container-sticky");
-    //         console.log("fully intersects with screen");
-    // }, { threshold: [0,1] });
+    // CHECK IF HEADER TOUCHES TOP SO THAT TO MOVE HAMBURGER
+    let observeEl = new IntersectionObserver(function(domEl) {
+        //console.log(entries);
+        //console.log(entries[0].target.id);
+        //console.log(entries[0].intersectionRatio);
+
+        if(domEl[0].intersectionRatio < 1) {
+            //console.log("no intersection with screen");
+
+            $(`.headerTop`).addClass('headerTopFixed');
+            $(`.headerTop .logoInitials a`).empty();
+            $(`.headerTop .logoInitials a`).append(`<img src="./img/logoBlack.png" alt="logo initials I.N.">`);
+            $(".borderBottom .socialMedia").css({'display': 'flex'});
+            $("#toTopLink").css({'display': 'block'});
+            $(`.headerTop .logoInitials`).removeClass('toggleOpacity');
+        }
+        else {
+            //console.log("fully intersects with screen");
+
+            $(`.headerTop`).removeClass('headerTopFixed');
+            $(`.headerTop .logoInitials a`).empty();
+            $(`.headerTop .logoInitials a`).append(`<img src="./img/logoWhite.png" alt="logo initials I.N.">`);
+            $(".borderBottom .socialMedia").css({'display': 'none'});
+            $("#toTopLink").css({'display': 'none'});
+        }
+
+    }, { threshold: [0,1] });
     
-    // observer.observe(document.querySelector("#headerTop"));
+    observeEl.observe(document.querySelector("#headerBottom"));
     
-    // WORKS BUT TOO MUCH LOAD ON THE SYSTEM?!
-
+    // WORKS BUT I THINK IT'S TOO MUCH LOAD ON THE SYSTEM
     // window.onscroll = () => {
     //     if (window.scrollY > 695) {
     //         $('#bottomSocialMedia').css({'display': 'flex'});
