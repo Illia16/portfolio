2f07041addfba5b505418b00fef05419fa543652
Illia <illia2143@gmail.com>
2020-10-05 23:28:26 -0400

    accessibility improvements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
+const myPortfolio = {};
 
+myPortfolio.loadingPage = function() {
     setTimeout(function() {
         $("#loadingPage").addClass('welcomeUp');
 
@@ -8,6 +9,15 @@ $(document).ready(function(){
         }, 1500);
     }, 1500);
 
+    // hiding all projects
+    $(`.projectsList li`).hide();
+    // showing only featured ones
+    $(`.projectsList li.featured`).show();
+    // making underline on featured ones by default
+    $(`.everySortButton:first-child`).addClass('onLoad');
+};
+
+myPortfolio.openNav = function() {
     // OPEN NAVIGATION MENU
     $('#openNav').click(function() {
         $('#nav').toggleClass('openedNav');
@@ -18,6 +28,20 @@ $(document).ready(function(){
         : $(`.headerTop .logoInitials`).removeClass('toggleOpacity')
     });
 
+    // nav focus loop
+    $('.socialMedia li:last-child a').focusout( function() {
+        $('.showNav').focus();
+    });
+
+    // putting focused element to the center of the screen
+        $('main a, main input, main textarea, main button').focus(function(){
+            let center = $(window).height()/2;
+            let top = $(this).offset().top ;
+            top > center && $(window).scrollTop(top-center);
+    });
+};
+
+myPortfolio.scrollTo = function() {
     // WHEN CLICKING ON A CERTAIN LINK WE GO TO the #element
     $("a.scrollTo").click(function (event) {
         event.preventDefault();
@@ -29,11 +53,9 @@ $(document).ready(function(){
         // bringing back our logo initials to 1(visible)
         $(`.headerTop .logoInitials`).removeClass('toggleOpacity');
     });
+};
 
-    $(`.projectsList li`).hide();
-    $(`.projectsList li.featured`).show();
-    $(`.everySortButton:first-child`).addClass('onLoad');
-
+myPortfolio.sortProjects = function() {
     $(".sortButtons button").click(function () {
         $(`.everySortButton:first-child`).removeClass('onLoad');
         $(`.projectsList li`).slideUp(300);
@@ -46,36 +68,69 @@ $(document).ready(function(){
         this.value === 'all' && $(`.projectsList li`).delay("300").slideDown(500);
             // this.value === 'all' && $(`.projectsList li`).delay(300).show(500);
     });
+};
+
+// setting the min-height of UL list of projects so that to always maintain gap with bottom background img
+myPortfolio.ulMinHeightSet = function() {
+    let ulMinHeight = $(`.projectImage`).height().toFixed(2) + 'px';
+    $('.projectsList').css({'min-height' : ulMinHeight });
+    // console.log('resized!!!!', ulMinHeight);
+    // console.log( $(window).width()    );
+};
 
 
-    // CHECK IF HEADER TOUCHES TOP SO THAT TO MOVE HAMBURGER
-    let observeEl = new IntersectionObserver(function(domEl) {
-        //console.log(entries);
-        //console.log(entries[0].target.id);
-        //console.log(entries[0].intersectionRatio);
 
-        if(domEl[0].intersectionRatio < 1) {
-            //console.log("no intersection with screen");
+// so that not to change the height every time width changes in EVERY PIXES, to prevent a huge load on the system, there's a fucntion with a step i
+$(window).on('resize', function(){
+    const arrSizes = [];
+    for (let i=250; i<=1500; i += 10) {
+        arrSizes.push(i);
+    }
 
-            $(`.headerTop`).addClass('headerTopFixed');
-            $(`.headerTop .logoInitials a`).empty();
-            $(`.headerTop .logoInitials a`).append(`<img src="./img/logoBlack.png" alt="logo initials I.N.">`);
-            $(".borderBottom .socialMedia").css({'display': 'flex'});
-            $("#toTopLink").css({'display': 'block'});
-            $(`.headerTop .logoInitials`).removeClass('toggleOpacity');
-        }
-        else {
-            //console.log("fully intersects with screen");
-            $(`.showNav span`).hasClass('clicked') && $(`.headerTop .logoInitials`).addClass('toggleOpacity')
+    if( arrSizes.includes( $(this).width() ) ) {
+        myPortfolio.ulMinHeightSet();
+    }
+});
 
-            $(`.headerTop`).removeClass('headerTopFixed');
-            $(`.headerTop .logoInitials a`).empty();
-            $(`.headerTop .logoInitials a`).append(`<img src="./img/logoWhite.png" alt="logo initials I.N.">`);
-            $(".borderBottom .socialMedia").css({'display': 'none'});
-            $("#toTopLink").css({'display': 'none'});
-        }
 
-    }, { threshold: [0,1] });
-    
-    observeEl.observe(document.querySelector("#headerBottom"));
+// CHECK IF HEADER TOUCHES TOP SO THAT TO MOVE HAMBURGER
+myPortfolio.observeEl = new IntersectionObserver(function(domEl) {
+    //console.log(entries);
+    //console.log(entries[0].target.id);
+    //console.log(entries[0].intersectionRatio);
+
+    if(domEl[0].intersectionRatio < 1) {
+        //console.log("no intersection with screen");
+
+        $(`.headerTop`).addClass('headerTopFixed');
+        $(`.headerTop .logoInitials`).empty();
+        $(`.headerTop .logoInitials`).append(`<img src="./img/logoBlack.png" alt="logo initials I.N.">`);
+        $(".borderBottom .socialMedia").css({'display': 'flex'});
+        $("#toTopLink").css({'display': 'block'});
+        $(`.headerTop .logoInitials`).removeClass('toggleOpacity');
+    }
+    else {
+        //console.log("fully intersects with screen");
+        $(`.showNav span`).hasClass('clicked') && $(`.headerTop .logoInitials`).addClass('toggleOpacity')
+
+        $(`.headerTop`).removeClass('headerTopFixed');
+        $(`.headerTop .logoInitials`).empty();
+        $(`.headerTop .logoInitials`).append(`<img src="./img/logoWhite.png" alt="logo initials I.N.">`);
+        $(".borderBottom .socialMedia").css({'display': 'none'});
+        $("#toTopLink").css({'display': 'none'});
+    }
+
+}, { threshold: [0,1] });
+
+myPortfolio.init = function() {
+    myPortfolio.loadingPage();
+    myPortfolio.ulMinHeightSet();
+    myPortfolio.openNav();
+    myPortfolio.scrollTo();
+    myPortfolio.sortProjects();
+    myPortfolio.observeEl.observe(document.querySelector("#headerBottom"));
+}
+
+$(document).ready(function(){
+    myPortfolio.init();
 });
